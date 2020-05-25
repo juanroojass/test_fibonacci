7dd57cce1154df35fdb68a8e5a898a702fd0d1c6
Juan <juan.roojass@gmail.com>
2020-05-25 18:45:47 -0500

    version 2

--- test_fibonacci/php/funcion_fibonacci.php
+++ test_fibonacci/php/funcion_fibonacci.php
@@ -5,8 +5,9 @@ fibonacci($valor);
 
 function fibonacci($valor){
 	if($valor>0 && $valor<=12){	
+		//for($x=1;$x<=$valor;$x++){
 		for($x=1;$x<=$valor;$x++){
-			print recursiva($x);		
+			print (recursiva($x) + recursiva($x+1) ).", ";		
 		}
 	}else{
 		print 'Ingrese un valor numérico mayor a cero y menor igual a 12';
@@ -16,12 +17,14 @@ function fibonacci($valor){
 
 function recursiva($v){
 	if ($v==0){
-		return 0 . ", ";
+		return 0 ;
 	}else if($v==1){
-		return 1 .", ";
+		return 1 ;
 	}
 	else{
-		return ( recursiva($v-1) + recursiva($v-2)  ).", ";
+
+		return ( recursiva($v-1) + recursiva($v-2) );
+		
 	}
 }
 
